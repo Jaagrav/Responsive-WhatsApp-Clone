0aa65ceb00b3b35e16fcc1f8a7e26f0a42d2010f
Jaagrav <jaagravseal03@gmail.com>
2020-09-02 20:13:28 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -255,7 +255,9 @@ window.addEventListener('resize', function () {
 })
 
 function resize() {
-    if (window.innerWidth < 450)
+    if (window.innerWidth > 768)
+        document.querySelector(".chat-box").style.transform = "translateX(0px)";
+    if (window.innerWidth < 768)
         document.querySelector(".menu-btn").src = "./menu-btn-mobile.png"
     else
         document.querySelector(".menu-btn").src = "./menu-btn.png";
